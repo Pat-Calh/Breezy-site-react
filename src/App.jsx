15c604af1d95bd10a762df21723dc350633fe5e3
PCalh <pat.calhoun97@gmai.com>
2025-06-30 09:02:34 -0500

    Updated Front Page Text

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Welcome to Breezy's New Site!</h1>
+      <p>Check back often for updates!</p>
+      <br />
+      <p>Currently working on the newest version of Breezy's site! </p>
+      <footer>
+        <p>© 2025 Breezy. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
